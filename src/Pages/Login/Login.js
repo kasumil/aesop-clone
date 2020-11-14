3d1891c7659abfb68dc58dacc27e92d3d0e8afa3
kasumil <ridou99@gmail.com>
2020-11-15 02:00:48 +0900

    [리팩토링] 로그인, 회원가입 함수형 리액트로 변경 완료

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,103 +1,93 @@
-import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import * as loginAPI from "../../config";
 import "./Login.scss";
 
-class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      pw: "",
-    };
-  }
+export default function Login() {
+  const [email, setEmail ] = useState('')
+  const [pw, setPw] = useState('')
 
-  hadleValueID = (e) => {
-    this.setState({
-      email: e.target.value,
-    });
-  };
+  const history = useHistory()
 
-  hadleValuePW = (e) => {
-    this.setState({
-      pw: e.target.value,
-    });
-  };
+  const hadleValueID = (e) => {
+    setEmail({[email]: e.target.value})};
+
+  const hadleValuePW = (e) => {
+    setPw({[pw]: e.target.value})};
 
   // 로그인용 버튼
-  hadleBtn = (e) => {
+  const hadleBtn = (e) => {
     fetch(loginAPI, {
       method: "POST",
       body: JSON.stringify({
-        email: this.state.email,
-        password: this.state.pw,
+        [email]: email,
+        [password]: pw,
       }),
     })
-      .then((res) => res.json())
-      .then((res) => {
-        if (res.token) {
-          localStorage.setItem("aesopToken", res.token);
-          //로그인시 nav에 email 전달하는 함수 (부모에게 전달)
-          this.props.showID(this.state.email);
-          this.props.handleLogin();
-        }
-      });
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.token) {
+        localStorage.setItem("aesopToken", res.token);
+        //로그인시 nav에 email 전달하는 함수 (부모에게 전달)
+        this.props.showID(email);
+        this.props.handleLogin();
+      }
+    })
+    .catch((e) => console.log(e.message))
+    .finally(console.log('로그인 정상작동'))
   };
 
   // 회원가입용
-  handleSignUp = (e) => {
-    this.props.history.push("/signup");
+  const handleSignUp = (e) => {
+    history.push("/signup");
   };
 
-  render() {
-    return (
-      <div className="Login">
-        <div className="modalBody">
-          <div className="introduceBox">
-            <h2 className="loginTitle">안녕하세요.</h2>
-            <div className="loginSub">유효한 이메일 주소를 입력하세요</div>
-          </div>
-          <div className="formText">
-            <label>
-              <input
-                onChange={this.hadleValueID}
-                className="formTextInput"
-                name="email"
-                placeholder="이메일 주소"
-              />
-              {/* <span className="FormTextLabel">이메일 주소</span> 이부분은 애니메이션이 요구됨. 해결방안이 시급. */}
-            </label>
-          </div>
-          <div className="formText">
-            <input
-              onChange={this.hadleValuePW}
-              className="formTextInput"
-              type="Password"
-              placeholder="비밀번호"
-            />
-          </div>
-          <button
-            onClick={this.hadleBtn}
-            type="button"
-            className={
-              this.state.email.length > 5 && this.state.pw.length > 5
-                ? "activeBTN"
-                : "unactiveBTN"
-            }
-          >
-            <div className="btnContent">로그인</div>
-          </button>
-          <button
-            onClick={this.handleSignUp}
-            type="button"
-            className="signUpcolor"
-          >
-            <div className="signUpBtn">회원가입</div>
-          </button>
-        </div>
+return (
+  <div className="Login">
+    <div className="modalBody">
+      <div className="introduceBox">
+        <h2 className="loginTitle">안녕하세요.</h2>
+        <div className="loginSub">유효한 이메일 주소를 입력하세요</div>
       </div>
-    );
-  }
+      <div className="formText">
+        <label>
+          <input
+            onChange={hadleValueID}
+            className="formTextInput"
+            name="email"
+            placeholder="이메일 주소"
+          />
+          {/* <span className="FormTextLabel">이메일 주소</span> 이부분은 애니메이션이 요구됨. 해결방안이 시급. */}
+        </label>
+      </div>
+      <div className="formText">
+        <input
+          onChange={hadleValuePW}
+          className="formTextInput"
+          type="Password"
+          placeholder="비밀번호"
+        />
+      </div>
+      <button
+        onClick={hadleBtn}
+        type="button"
+        className={
+          email.length > 5 && pw.length > 5
+            ? "activeBTN"
+            : "unactiveBTN"
+        }
+      >
+        <div className="btnContent">로그인</div>
+      </button>
+      <button
+        onClick={handleSignUp}
+        type="button"
+        className="signUpcolor"
+      >
+        <div className="signUpBtn">회원가입</div>
+      </button>
+    </div>
+  </div>
+);
 }
 
-export default withRouter(Login);
--- src/Pages/Login/Signup.js
+++ src/Pages/Login/Signup.js
@@ -1,206 +1,187 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import * as signUpAPI from "../../config";
-import { withRouter } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import "./Signup.scss";
 
-class Signup extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      pw: "",
-      pwconfirm: "",
-      firstname: "",
-      lastname: "",
-      pwError: false,
-    };
-  }
+export default function Signup() {
+  const [ email, setEamil ] = useState('')
+  const [ pw, setPw ] = useState('')
+  const [ pwconfirm, setPwonfirm ] = useState('')
+  const [ firstname, setFirstname ] = useState('')
+  const [ lastname, setLastname ] = useState('')
+  const [ pwError, setPwError ] = useState(false)
 
-  hadleValueEmail = (e) => {
-    this.setState({
-      email: e.target.value,
-    });
-  };
+  const { history } = useHistory
 
-  hadleValuePW = (e) => {
-    this.setState({
-      pw: e.target.value,
-    });
-  };
+  const hadleValueEmail = (e) => {
+    setEamil({ [email]: e.target.value})};
 
-  hadleValuepwConFirm = (e) => {
-    this.setState({
-      pwconfirm: e.target.value,
-    });
+  const hadleValuePW = (e) => {
+    setPw({[pw]: e.target.value})};
 
+  const hadleValuepwConFirm = (e) => {
+    setPwonfirm({[pwconfirm]: e.target.value});
     if (e.target.value.length > 0) {
-      if (e.target.value === this.state.pw) {
-        this.setState({ pwError: false });
+      if (e.target.value.value === pw) {
+        setPwError({ [pwError]: false });
       } else {
-        this.setState({ pwError: true });
+        setPwError({ [pwError]: true });
       }
     }
   };
 
-  hadleValueFirstName = (e) => {
-    this.setState({
-      firstname: e.target.value,
-    });
+  const hadleValueFirstName = (e) => {
+    setFirstname({[firstname]: e.target.value})
   };
 
-  hadleValueLastName = (e) => {
-    this.setState({
-      lastname: e.target.value,
-    });
+  const hadleValueLastName = (e) => {
+    setLastname({[lastname]: e.target.value})
   };
 
-  handleSingUP = (e) => {
+  const handleSingUP = (e) => {
     fetch(signUpAPI, {
       method: "POST",
       body: JSON.stringify({
-        email: this.state.email,
-        password: this.state.pw,
-        first_name: this.state.firstname,
-        last_name: this.state.lastname,
+        [email]: email,
+        [password]: pw,
+        [first_name]: firstname,
+        [last_name]: lastname,
       }),
     })
-      .then((res) => res.json())
-      .then((res) => {
-        if (res.token) {
-          localStorage.setItem("aesop", res.token);
-          alert("회원가입을 환영합니다.");
-          this.props.history.push("/");
-        } else {
-          alert("이메일과 비밀번호를 확인해주세요.");
-        }
-      });
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.token) {
+        localStorage.setItem("aesop", res.token);
+        alert("회원가입을 환영합니다.");
+        history.push("/");
+      } else {
+        alert("이메일과 비밀번호를 확인해주세요.");
+      }
+    })
+    .catch((e)=> console.log(e.message));
   };
 
-  handleHaveAccount = (e) => {
-    this.props.history.push("/Login");
-  };
+  const handleHaveAccount = (e) => {history.push("/Login")};
 
-  render() {
-    return (
-      <div className="Signup">
-        <form className="loginForm">
-          <div className="modalheadingWrap">
-            <h2 className="modalTitle">
-              처음 만나 뵙게 되네요. 이솝에 오신 것을 환영합니다.
-            </h2>
-            <div className="modalSubTitle">
-              계정을 만들려면 아래에 세부 정보를 입력하십시오.
+  return(
+    <div className="Signup">
+      <form className="loginForm">
+        <div className="modalheadingWrap">
+          <h2 className="modalTitle">
+            처음 만나 뵙게 되네요. 이솝에 오신 것을 환영합니다.
+          </h2>
+          <div className="modalSubTitle">
+            계정을 만들려면 아래에 세부 정보를 입력하십시오.
+          </div>
+        </div>
+        <div className="formRow">
+          <div className="formText">
+            <label>
+              <input
+                onChange={(e)=>{hadleValueEmail}}
+                className="formTextInput"
+                name="email"
+                placeholder="이메일 주소"
+                type="email"
+              />
+              {/* <span className="FormTextlabel">이메일 주소</span> */}
+            </label>
+          </div>
+        </div>
+        <div>
+          <div className="formRow">
+            <div className="formText">
+              <label>
+                <input
+                  onChange={(e)=>{hadleValuePW}}
+                  className="formTextInput"
+                  name="password"
+                  placeholder="패스워드"
+                  type="password"
+                />
+                {/* <span className="FormText-label">패스워드</span> */}
+              </label>
             </div>
           </div>
           <div className="formRow">
             <div className="formText">
               <label>
                 <input
-                  onChange={this.hadleValueEmail}
+                  onChange={(e)=>{hadleValuepwConFirm}}
                   className="formTextInput"
-                  name="email"
-                  placeholder="이메일 주소"
-                  type="email"
+                  name="passwordConfirm"
+                  placeholder="패스워드 확인"
+                  type="password"
                 />
-                {/* <span className="FormTextlabel">이메일 주소</span> */}
+                <div
+                  className={
+                    pwError
+                      ? "errorMessage"
+                      : "noneErrorTextMessage"
+                  }
+                >
+                  이전에 사용했던 패스워드를 입력하세요.
+                </div>
+                {/* <span className="FormText-label">패스워드 확인</span> */}
               </label>
             </div>
           </div>
-          <div>
-            <div className="formRow">
-              <div className="formText">
-                <label>
-                  <input
-                    onChange={this.hadleValuePW}
-                    className="formTextInput"
-                    name="password"
-                    placeholder="패스워드"
-                    type="password"
-                  />
-                  {/* <span className="FormText-label">패스워드</span> */}
-                </label>
-              </div>
+          <div className="formRowbox">
+            <div className="formTextLoginName">
+              <label>
+                <input
+                  onChange={(e)=>{hadleValueFirstName}}
+                  className="formTextInput"
+                  maxLength="100"
+                  name="firstName"
+                  placeholder="성"
+                  type="text"
+                />
+                {/* <span className="FormText-label">성</span> */}
+              </label>
             </div>
-            <div className="formRow">
-              <div className="formText">
-                <label>
-                  <input
-                    onChange={this.hadleValuepwConFirm}
-                    className="formTextInput"
-                    name="passwordConfirm"
-                    placeholder="패스워드 확인"
-                    type="password"
-                  />
-                  <div
-                    className={
-                      this.state.pwError
-                        ? "errorMessage"
-                        : "noneErrorTextMessage"
-                    }
-                  >
-                    이전에 사용했던 패스워드를 입력하세요.
-                  </div>
-                  {/* <span className="FormText-label">패스워드 확인</span> */}
-                </label>
-              </div>
-            </div>
-            <div className="formRowbox">
-              <div className="formTextLoginName">
-                <label>
-                  <input
-                    onChange={this.hadleValueFirstName}
-                    className="formTextInput"
-                    maxLength="100"
-                    name="firstName"
-                    placeholder="성"
-                    type="text"
-                  />
-                  {/* <span className="FormText-label">성</span> */}
-                </label>
-              </div>
-              <div className="formTextLoginName">
-                <label>
-                  <input
-                    onChange={this.hadleValueLastName}
-                    className="formTextInput"
-                    maxLength="100"
-                    name="LastName"
-                    placeholder="이름"
-                    type="text"
-                  />
-                  {/* <span className="FormText-label">이름</span> */}
-                </label>
-              </div>
+            <div className="formTextLoginName">
+              <label>
+                <input
+                  onChange={(e)=>{hadleValueLastName}}
+                  className="formTextInput"
+                  maxLength="100"
+                  name="LastName"
+                  placeholder="이름"
+                  type="text"
+                />
+                {/* <span className="FormText-label">이름</span> */}
+              </label>
             </div>
           </div>
+        </div>
+        <button
+          onClick={(e)=>{handleSingUP}}
+          className={
+            email.length > 5 &&
+            pw === pwconfirm &&
+            firstname.length >= 1 &&
+            lastname.length >= 1
+              ? "btnSignUPActive"
+              : "btnSignUPunActive"
+          }
+          type="submit"
+        >
+          <div className="btnContent">
+            <span className="btnLabel">등록</span>
+          </div>
+        </button>
+        <div className="loginHaveAccount">
           <button
-            onClick={this.handleSingUP}
-            className={
-              this.state.email.length > 5 &&
-              this.state.pw === this.state.pwconfirm &&
-              this.state.firstname.length >= 1 &&
-              this.state.lastname.length >= 1
-                ? "btnSignUPActive"
-                : "btnSignUPunActive"
-            }
-            type="submit"
+            onClick={(e)=>{handleHaveAccount}}
+            className="loginHaveAccountBTN"
           >
-            <div className="btnContent">
-              <span className="btnLabel">등록</span>
-            </div>
+            이솝 계정을 가지고 계십니까?
           </button>
-          <div className="loginHaveAccount">
-            <button
-              onClick={this.handleHaveAccount}
-              className="loginHaveAccountBTN"
-            >
-              이솝 계정을 가지고 계십니까?
-            </button>
-          </div>
-        </form>
-      </div>
-    );
-  }
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default withRouter(Signup);
+
