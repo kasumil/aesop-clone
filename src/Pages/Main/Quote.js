34af787b3b6e668719420a5b09e0553fe2dc0bd1
kasumil <ridou99@gmail.com>
2020-11-19 21:47:12 +0900

    [modify] 메인 함수형 컴포넌트로 전환

--- src/Pages/Main/Quote.js
+++ src/Pages/Main/Quote.js
@@ -1,18 +1,15 @@
 import React from "react";
 import "./Quote.scss";
 
-class Quote extends React.Component {
-  render() {
-    return (
-      <div className="Quote">
-        <blockquote className="quoteContent">
-          <p className="quoteParagraph">
-            ‘Know'st thou the land where the lemon-trees bloom?’
-          </p>
-          <p className="quoteAuthor">Johann Wolfgang von Goethe</p>
-        </blockquote>
-      </div>
-    );
-  }
+export default function Quote() {
+return (
+    <div className="Quote">
+      <blockquote className="quoteContent">
+        <p className="quoteParagraph">
+          ‘Know'st thou the land where the lemon-trees bloom?’
+        </p>
+        <p className="quoteAuthor">Johann Wolfgang von Goethe</p>
+      </blockquote>
+    </div>
+  );
 }
-export default Quote;
